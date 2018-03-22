c7259ac9829f35b4e1200eab71587a50f03abd14
TheAstroKnight <knightgeekyt@gmail.com>
2018-03-22 17:38:02 +0100

    Getting the mcmod.info not found error again...

---
+++ src/main/java/me/astroknight/forgeofindustries/ForgeOfIndustries.java
@@ -0,0 +1,21 @@
+package me.astroknight.forgeofindustries;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = "forgeofindustries", useMetadata = true)
+public class ForgeOfIndustries {
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+    }
+}
